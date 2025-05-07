19f0091b0c5c118b8d5198289d1c8384d6d352f9
GUILHERME GUEDERT <guilherme_guedert@estudante.sesisenai.org.br>
2025-05-07 09:25:38 -0300

    adicionando rota logs

--- script.js
+++ script.js
@@ -1,33 +1,60 @@
-//importacões
+// importações
 import fs from 'fs';
 import { v4 } from 'uuid';  
-import express from 'express'
+import express from 'express';
 
-const app = express()
-const porta = 3000
+const app = express();
+const porta = 3000;
 const path = "logs.txt";
-const novoID = gerarID();
-const dataHoraAtual = new Date();
-const conteudoArquivo = `${novoID} - ${dataHoraAtual.toISOString()} - Guilherme Guedert\n`; // formata como uma string o conteudo do arquivo
 
-//gerar id
+app.use(express.json());
+
+// Função para gerar ID
 function gerarID() { 
     return v4();  
 }
 
-//adicionar mensagem no arquivo
-function adicionarMensagens() {
-    fs.appendFile(path, conteudoArquivo, (err) => { //escreve ao final do arquivo
+// funcao para adicionar mensagens no arquivo
+function adicionarMensagens(conteudoArquivo) {
+    fs.appendFile(path, conteudoArquivo, (err) => { // escreve ao final do arquivo
         if (err) {
-            console.error("Erro ao escrever no arquivo:", err);
+            console.error("Erro ao registrar log", err);
         } else {
             console.log("Mensagem adicionada com sucesso!");
         }
     });
 }
 
-app.listen(porta,()=>{
-    console.log(`ouvindo na porta ${porta}`)
-} )
+// rota POST /logs
+app.post('/logs', (req, res) => {
+    // recebe o nome do aluno do corpo da requisição
+    const { nomeAluno } = req.body;
+    
+    // caso não tenha nomeAluno
+    if (!nomeAluno) {
+        return res.status(400).json({ mensagem: 'Insira o nome do aluno' });
+    }
 
-adicionarMensagens();
+    // gera um novo ID
+    const novoID = gerarID();
+
+    // obtém a data e hora atual
+    const dataHoraAtual = new Date().toISOString();
+
+    // formata a mensagem para o arquivo
+    const conteudoArquivo = `${novoID} - ${dataHoraAtual} - ${nomeAluno}\n`;
+
+    // adiciona a mensagem em outra linha do arquivo
+    adicionarMensagens(conteudoArquivo);
+
+    // retorna o ID gerado e a mensagem de sucesso
+    res.status(200).json({
+        id: novoID,
+        mensagem: 'Log registrado com sucesso!',
+    });
+});
+
+// teste para ver se está ouvindo na porta correta
+app.listen(porta, () => {
+    console.log(`Servidor ouvindo na porta ${porta}`);
+});
